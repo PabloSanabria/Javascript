d4543feb83385fa188135b452a3a374ada422edf
Pablo Sanabria <pablosanabria_1986@hotmail.com>
2024-03-15 15:00:58 -0300

    ES6 Sets

--- Javascript-ES6/03-Javascript/app.js
+++ Javascript-ES6/03-Javascript/app.js
@@ -14,15 +14,51 @@ let userID = Symbol.for("userID");
 let objeto = {};
 
 objeto[userID] = "12345";
-console.log(objeto[userID], userID)
+// console.log(objeto[userID], userID)
 
 //hacer referencia al simbolo creado
 let userID2 = Symbol.for("userID");//este nuevo simbolo apunta a la referencia del anterior
 
 //obtener el id de un simbolo
 let idSimbolo = Symbol.for("id unico");
-console.log(Symbol.keyFor(idSimbolo));
+// console.log(Symbol.keyFor(idSimbolo));
 
 let id3 = Symbol("id unico"); //si creamos unpo nuevo con el mismo id 
-console.log(id3 === idSimbolo);//son distintos 
-console.log(id3,idSimbolo);
+// console.log(id3 === idSimbolo);//son distintos 
+// console.log(id3,idSimbolo);
+//---
+let items = new Set();
+//ignora valores repetidos
+items.add(10);
+items.add(10);
+items.add(11);
+items.add(7);
+items.add("7");//esto si lo permite xq internamente es un object
+
+//console.log(items, items.size);
+let items2 = new Set([1,2,3,4,5,7]);
+//console.log(items2.has(7));//devuelve true o false
+//borrar elementos
+console.log(items2, items2.size);
+items2.delete(3);//compara con el contenido, no con la posicion, si no existe lo ignora
+//console.log(items2, items2.size);
+items2.clear();//elimina todos los elementos
+//console.log(items2);
+//recorrer sets
+let personas = new Set(["Fernand", "PAblo", "Susana"]);
+personas.forEach( function(valor, llave, setOriginal){
+    //console.log(valor, llave, setOriginal);
+}
+);
+//Convertir Set en array
+let numeros = [1,2,3,2,4,7,5,6,7];
+let setNumeros = new Set(numeros);
+//console.log(setNumeros);
+let arrNumeros = [...setNumeros];
+//console.log(arrNumeros);
+//Ej elimnacion de duplicados en array
+function eliminarDuplicados(items){
+    return [... new Set(items)];
+}
+
+console.log(eliminarDuplicados(numeros));
