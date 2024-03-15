bcd682e1fbd122f9f2d09e1b5190edecaf604514
Pablo Sanabria <pablosanabria_1986@hotmail.com>
2024-03-15 16:23:03 -0300

    ES6 Maps

--- Javascript-ES6/03-Javascript/app.js
+++ Javascript-ES6/03-Javascript/app.js
@@ -39,7 +39,7 @@ items.add("7");//esto si lo permite xq internamente es un object
 let items2 = new Set([1,2,3,4,5,7]);
 //console.log(items2.has(7));//devuelve true o false
 //borrar elementos
-console.log(items2, items2.size);
+//console.log(items2, items2.size);
 items2.delete(3);//compara con el contenido, no con la posicion, si no existe lo ignora
 //console.log(items2, items2.size);
 items2.clear();//elimina todos los elementos
@@ -61,4 +61,32 @@ function eliminarDuplicados(items){
     return [... new Set(items)];
 }
 
-console.log(eliminarDuplicados(numeros));
+//console.log(eliminarDuplicados(numeros));
+//--Maps
+let mapa = new Map();
+mapa.set("nombre","Fernando");
+mapa.set("edad",31);
+mapa.set("apellido");// si no colocamos valor por defecto queda undefined
+//console.log(mapa);
+//console.log(mapa.get("nombre"));//obtener valor pasando key
+//console.log(mapa.has("nombre"));//verifica existencia devuelve true o false
+mapa.delete("nombre");//borra todo el par
+mapa.clear("nombre");//elimina todos los valores del mapa
+//console.log(mapa);
+//--inicializar por defecto
+let mapa2 = new Map([ ["nombre","Fernando"],["edad",31] ]);
+//console.log(mapa2);
+//recorrer un mapa
+// mapa2.forEach(function(valor,llave, mapaOrigen){
+//     console.log(`LLave: ${llave}, valor: ${valor}`);
+//     console.log(mapaOrigen);
+// })
+//mapa2.forEach( (valor, llave) => console.log(`LLave: ${llave}, valor: ${valor}`)) 
+
+//--Ciclo For Of 
+//let arrNuevo = [41,72,93,52,24,77,95,46,7];
+for( let i of mapa2){
+    console.log(i);
+}
+let mapa3 = new Map();
+console.log(mapa3);
