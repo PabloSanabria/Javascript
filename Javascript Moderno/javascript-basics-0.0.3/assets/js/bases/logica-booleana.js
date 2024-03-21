69840ecf933c158aa886ec698a83ca677f2d3d3c
Pablo Sanabria <pablosanabria_1986@hotmail.com>
2024-03-21 16:27:16 -0300

    Actualizacion docu y codigo de ejemplos

---
+++ Javascript Moderno/javascript-basics-0.0.3/assets/js/bases/logica-booleana.js
@@ -0,0 +1,67 @@
+const regresaTrue = () => {
+    console.log('Regresa true');
+    return true;
+}
+
+const regresaFalse = () => {
+    console.log('Regresa false');
+    return false;
+}
+
+console.warn('Not o la negación');
+console.log( true ); // true
+console.log( !true ); // false
+console.log( !false ); // true
+
+console.log( !regresaFalse() ); // true
+
+
+console.warn('And'); // true si todos los valores son verdaderos
+console.log( true && true ); // true
+console.log( true && !false ); // true
+
+console.log('=========');
+console.log(  regresaFalse() && regresaTrue() ); // false
+console.log(  regresaTrue() && regresaFalse() ); // false
+
+console.log('==== && =====');
+regresaFalse() && regresaTrue();
+
+console.log( '4 condiciones ', true && true && true && false ); // false
+
+
+console.warn('OR'); // true
+console.log( true || false );
+console.log( false || false );
+
+console.log( regresaTrue() || regresaFalse() );
+console.log( '4 condiciones ', true || true || true || false ); // true
+
+
+
+console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+//ESTO ES POCO COMUN pero existe
+// como es un and si la primera condicion es false sigue y si la segunda tiene lo asigna y asi...
+const a1 = false && 'Hola Mundo' && 150; //devuelve a1=150
+// devuelve false de soyFalso ya que al ver este falso deja de recorrer la condicion 
+const a2 = 'Hola' && 'Mundo' && soyFalso && true; 
+//por contrario con el or en este caso si encuentra un false sigue recorriendo la condicion hasta enconterar un true 
+const a3 = soyFalso || 'Ya no soy falso'; //devuelve "Ya no soy falso"
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+
+console.log({a1, a2, a3, a4, a5 });
+
+if ( regresaFalse() && regresaTrue() && (true || false || true) ) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
+
+
+
+
